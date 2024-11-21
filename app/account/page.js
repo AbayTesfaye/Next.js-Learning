e2b35d5a6d2dece0abf854f12521e4b8ae0483b1
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-22 01:53:20 +0300

    use useSearchParams to access key-value of url

--- app/account/page.js
+++ app/account/page.js
@@ -4,7 +4,7 @@ export default function Accounts() {
   const accountsInfo = null;
 
   if (accountsInfo === null) {
-    redirect('/profile'); // Server-side redirection
+    redirect('/cart?search=product1&randomvalue=abcd'); // Server-side redirection
   }
 
   return <div>This is Accounts page</div>;
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,9 +1,12 @@
 'use client'
-import { usePathname } from "next/navigation"
+import { usePathname, useSearchParams } from "next/navigation"
 
 export default function Cart(){
     const pathname = usePathname();
     console.log(pathname);
+
+    const searchParams = useSearchParams();
+    console.log(searchParams.get('search'),searchParams.get('randomvalue'));
     return (
         <div>
             <h1>This is cart page!</h1>
