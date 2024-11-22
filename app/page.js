83a227b7385b2cc1774c6d06bbe4a368751d2c98
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-22 14:07:43 +0300

    Navigate to other page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
   return (
-    <div>Welcome to next js 2024</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-6">Welcome to Next.js 2024</h1>
+      <Link href="/product">
+      Go to Product
+      </Link>
+      <Link href="/account/membership">
+       Go to Account
+      </Link>
+    </div>
+  );
 }
