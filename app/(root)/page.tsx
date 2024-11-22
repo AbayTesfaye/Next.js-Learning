d5aa80c094e24ca859b5c84ffa99bd0ae480eb9d
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-22 13:42:23 +0300

    change files to js

--- app/(root)/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
- console.log("what am i doning here...");  
-  return (
-    <h1 className="text-3xl">Welcome next.js</h1>
-  );
-}
---
+++ app/product/[details]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Details() {
+  return (
+    <div>This is Details page!</div>
+  )
+}
