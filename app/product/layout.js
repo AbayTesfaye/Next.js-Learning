ae4f8b098754ecc9428960e49d3d9aa38bbc1e7e
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-23 21:59:51 +0300

    about not-found to handle 404 error

--- app/product/layout.js
+++
@@ -1,8 +0,0 @@
-import { Suspense } from "react";
-import Loading from "../loading"
-
-export default function ProductLayout({children}){
-    return <h1>
-           <Suspense fallback={<Loading/>}> {children}</Suspense>
-    </h1>
-}
