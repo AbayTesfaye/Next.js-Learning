4e8114879421e6ec72b78931c414dbbb082e29c0
AbayTesfaye <abaytesfaye02@gmail.com>
2024-12-01 13:36:28 +0300

    understand client and server components

--- app/counter.js
+++ app/counter.js
@@ -4,13 +4,36 @@ import { useState } from "react";
 
 export default function counter() {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState();
   return (
     <div className="flex flex-col justify-center items-center ">
       <h1>Count : {count} Times</h1>
+      <div>Boolean Value : {value}</div>
       <div className="flex justify-center items-center gap-6">
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-        <button onClick={() => setCount(0)}>Reset</button>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            setValue("Positive");
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+            setValue("Negative");
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            setCount(0);
+            setValue("null");
+          }}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
