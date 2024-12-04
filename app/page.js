41b91e617b690146ec61e423b620a83da9536360
AbayTesfaye <abaytesfaye02@gmail.com>
2024-12-04 17:56:11 +0300

    About Loading component

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 "use client"; // Ensures the component runs on the client
-
-import Link from "next/link"; // Corrected import for useRouter in Next.js 13+ app directory
-import React from "react";
+import Loading from "./loading";
+import Link from "next/link"; // Import for navigation
+import React, { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Welcome to Next.js 2024</h1>
-      <Link href="/about">About</Link>
-      <Link href="/product">Product</Link>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold mb-6">Welcome to Next.js 2024</h1>
+        <Link href="/about">About</Link>
+        <Link href="/product">Product</Link>
+      </div>
+    </Suspense>
   );
 }
