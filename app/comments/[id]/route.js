bdaf33d64042990f51f2b5e8c44c57fe6c0ba7b7
AbayTesfaye <abaytesfaye02@gmail.com>
2024-12-06 05:00:10 -0800

    PATCH route handler

--- app/comments/[id]/route.js
+++ app/comments/[id]/route.js
@@ -1,8 +1,45 @@
 import { comments } from "../data";
 
 export async function GET(request, { params }) {
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
-  return Response.json(comment);
+  const commentId = parseInt(params.id, 10);
+
+  const comment = comments.find((comment) => comment.id === commentId);
+
+  if (!comment) {
+    return new Response(JSON.stringify({ error: "Comment not found" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return new Response(JSON.stringify(comment), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function PATCH(request, { params }) {
+  const commentId = parseInt(params.id, 10);
+
+  // Parse the request body
+  const body = await request.json();
+  const { text } = body;
+
+  // Find the index of the comment
+  const index = comments.findIndex((comment) => comment.id === commentId);
+
+  // If the comment doesn't exist, return a 404 response
+  if (index === -1) {
+    return new Response(JSON.stringify({ error: "Comment not found" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  // Update the comment text
+  comments[index].text = text;
+
+  // Return the updated comment
+  return new Response(JSON.stringify(comments[index]), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
