78cdb8d58dff30ddc3b67efc8fd938c78ec465ad
AbayTesfaye <abaytesfaye02@gmail.com>
2024-12-06 05:11:56 -0800

    handling DELETE request

--- app/comments/[id]/route.js
+++ app/comments/[id]/route.js
@@ -1,45 +1,27 @@
 import { comments } from "../data";
 
 export async function GET(request, { params }) {
-  const commentId = parseInt(params.id, 10);
-
-  const comment = comments.find((comment) => comment.id === commentId);
-
-  if (!comment) {
-    return new Response(JSON.stringify({ error: "Comment not found" }), {
-      status: 404,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-
-  return new Response(JSON.stringify(comment), {
-    headers: { "Content-Type": "application/json" },
-  });
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  return Response.json(comment);
 }
 
 export async function PATCH(request, { params }) {
-  const commentId = parseInt(params.id, 10);
-
-  // Parse the request body
   const body = await request.json();
   const { text } = body;
-
-  // Find the index of the comment
-  const index = comments.findIndex((comment) => comment.id === commentId);
-
-  // If the comment doesn't exist, return a 404 response
-  if (index === -1) {
-    return new Response(JSON.stringify({ error: "Comment not found" }), {
-      status: 404,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-
-  // Update the comment text
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
   comments[index].text = text;
+  return Response.json(comment[index]);
+}
 
-  // Return the updated comment
-  return new Response(JSON.stringify(comments[index]), {
-    headers: { "Content-Type": "application/json" },
-  });
+export async function DELETE(request, { params }) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
 }
