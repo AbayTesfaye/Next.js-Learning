b23dadf9e5b459b6d864d1b58b28be61546992cf
AbayTesfaye <abaytesfaye02@gmail.com>
2024-12-07 15:36:42 +0300

    understand more about redirect@

--- app/product/[productId]/error.js
+++ app/product/[productId]/error.js
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
 
-export default function Error({ error }) {
+export default function Error({ error, reset }) {
   return (
     <div>
       <h1>Oops! Something went wrong.</h1>
       <p>{error.message}</p>
+      <button onClick={reset}>Try Again</button>
     </div>
   );
 }
